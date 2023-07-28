7f02febcc6e3a1b47845a773c568f10230f24a96
Hande Gul Atasagun <84257643+hgatasagun@users.noreply.github.com>
2023-07-28 22:03:36 +0300

    chipotle.py

--- chipotle.py
+++ chipotle.py
@@ -38,7 +38,7 @@ df.head(20)
 ### Data understanding
 ##############################################
 def check_df(dataframe, head=5):
-    print('################# Shape ################# ')
+    print('################# Columns ################# ')
     print(dataframe.columns)
     print('################# Types  ################# ')
     print(dataframe.dtypes)
